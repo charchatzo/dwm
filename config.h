9f63329e1d1768b0bbca0cde43dceaedfdf912fa
CharChatzo <charchatzo2008@gmail.com>
2022-01-27 11:39:02 +0200

    Clean up, switch default terminal to kitty, change color pallete and fonts, fonts are now JetBrains Mono and JoyPixels for the emojis, scaled the font size down a bit

--- config.h
+++ config.h
@@ -5,13 +5,13 @@ static const unsigned int borderpx  = 1;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "Source Code Pro:style=Semibold:size=12" };
+static const char *fonts[]          = { "JetBrains Mono:size=11", "JoyPixels:pixelsize=11:antialias=true:autohint=true" };
 static const char dmenufont[]       = "Source Code Pro:style=Semibold:size=12";
 static const char col_gray1[]       = "#1d1f21";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#c5c8c6";
 static const char col_gray4[]       = "#c5c8c6";
-static const char col_cyan[]        = "#a54242";
+static const char col_cyan[]        = "#212e38";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
 	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
@@ -58,11 +58,11 @@ static const Layout layouts[] = {
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
-static const char *termcmd[]  = { "st", NULL };
+static const char *termcmd[]  = { "kitty", NULL };
 
 static Key keys[] = {
 	/* modifier                     key        function        argument */
-	{ MODKEY,                       XK_d,      spawn,          SHCMD("rofi -show drun")},
+	{ MODKEY,                       XK_d,      spawn,          {.v = dmenucmd}},
 	{ MODKEY,                       XK_Return, spawn,          {.v = termcmd } },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
